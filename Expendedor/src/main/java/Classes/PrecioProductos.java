edb6406884cfe84f33c93de68a9dc583833805fe
sshiro0 <francnunez2023@inf.udec.cl>
2024-05-24 13:29:21 -0400

    Cambios en los precios de los productos.

--- Expendedor/src/main/java/Classes/PrecioProductos.java
+++ Expendedor/src/main/java/Classes/PrecioProductos.java
@@ -4,27 +4,24 @@ package Classes;
  * Enum que representa el valor de cada producto que se almacenan dentro de la clase Expendedor
  */
 public enum PrecioProductos {
-    VALORCOCACOLA(1000),
-    VALORSPRITE(800),
-    VALORFANTA(700),
-    VALORSNICKERS(600),
-    VALORSUPER8(200);
+    VALORCOCACOLA(1200),
+    VALORSPRITE(1000),
+    VALORFANTA(900),
+    VALORSNICKERS(800),
+    VALORSUPER8(300);
 
     private final int precio;
-
     /**
-     * Constructor del Enum, inicializa la variable precio
-     *
-     * @param precio Precio del producto pedido
+     * Constructor del Enum, inicializa la variable precio.
+     * @param precio Precio del producto pedido.
      */
     private PrecioProductos(int precio){
         this.precio = precio;
     }
 
     /**
-     * Obtiene el precio del producto
-     *
-     * @return entero con el precio del producto
+     * Obtiene el precio del producto.
+     * @return entero con el precio del producto.
      */
     public int getPrecio(){
        return precio;
--- Expendedor/src/main/java/Classes/Productos.java
+++ Expendedor/src/main/java/Classes/Productos.java
@@ -1,7 +1,7 @@
 package Classes;
 
 /**
- * Enum que representa los productos que son almacenados en los depositos de la clase Expendedor
+ * Enum que representa los productos que son almacenados en los depositos de la clase Expendedor.
  */
 public enum Productos {
     COCA,
