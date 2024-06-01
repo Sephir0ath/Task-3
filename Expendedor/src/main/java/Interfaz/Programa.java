b21d67ecf0285176a08d21c95b7e667c17652b82
Mazulin <matijenven2004@gmail.com>
2024-06-01 18:28:29 -0400

    Creacion de clase Programa, sera el encargado de incializar la aplicacion.

---
+++ Expendedor/src/main/java/Interfaz/Programa.java
@@ -0,0 +1,14 @@
+package Interfaz;
+
+/**
+ * Clase que inicializa la interfaz gráfica y el programa en general.
+ */
+public class Programa {
+    /**
+     * Ejecuta la Ventana.
+     * @param args por defecto.
+     */
+    public static void main(String[] args) {
+        Window.frame();
+    }
+}
