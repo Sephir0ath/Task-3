9d1ca3ee78e73037460048e77ab99c88e5811d94
Kingsephir <feliperaysz3@gmail.com>
2024-06-01 19:43:09 -0400

    Cambios en la lógica de comprar, corrección de errores, creación método addVuelto y consumir (Falta añadir javadocs).

--- Expendedor/src/main/java/Classes/Comprador.java
+++ Expendedor/src/main/java/Classes/Comprador.java
@@ -8,9 +8,9 @@ import Excepciones.PagoInsuficienteException;
  * Esta clase representa al Comprador el cual interactúa con el Expendedor e intenta comprar un producto.
  */
 public class Comprador {
-    private String sonido;
-    private int vuelto;
-    private Producto producto;
+    private String sonido = null;
+    private int vuelto = 0;
+    private Producto producto = null;
 
     /**
      * Constructor por defecto.
@@ -21,31 +21,30 @@ public class Comprador {
 
     /**
      * Método que representa el comprar un producto.
+     * @param moneda Moneda utilizada para comprar.
      * @param cualProducto Producto que el comprador solicita comprar.
      * @param expendedor Expendedor que contiene los productos para realizar la compra.
      * @throws NoHayProductoException Caso que no está el producto solicitado (deposito vacío o no existe el producto).
      * @throws PagoInsuficienteException Caso en que el valor de la moneda es menor al precio del producto.
      * @throws PagoIncorrectoException Caso en que la moneda es nula.
      */
-    public void comprar(Productos cualProducto, Expendedor expendedor) throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
+    public void comprar(Moneda moneda, Productos cualProducto, Expendedor expendedor) throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
         try{
-            expendedor.comprarProducto(cualProducto);
+            expendedor.comprarProducto(moneda, cualProducto);
             producto = expendedor.getProducto();
-
-            while(true) {
-                Moneda monedaAObtener = expendedor.getVuelto();
-                if (monedaAObtener == null) {
-                    break;
-                }
-                vuelto += monedaAObtener.getValor();
-                
-            }
-
         }
 
         catch(NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e){
             throw e;
         }
+    }
+
+    /**
+     * Agrega vuelto al total del vuelto.
+     * @param valor cantidad de vuelto agregada al total.
+     */
+    public void addVuelto (int valor){
+        vuelto += valor;
     }
 
     /**
@@ -56,12 +55,19 @@ public class Comprador {
         return vuelto;
     }
 
+    public void consumir(){
+        sonido = producto.consumir();
+    }
+
     /**
      * Método que entrega el nombre de el producto consumido
      * @return string sonido con el nombre del producto
      */
     public String queConsumiste() {
-        sonido = producto.consumir();
         return sonido;
+    }
+
+    public Producto getProducto() {
+        return producto;
     }
 }
