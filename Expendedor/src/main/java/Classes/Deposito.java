3899cf4f841a5e182ae29bab76d122f60a7df991
Kingsephir <feliperaysz3@gmail.com>
2024-06-01 19:45:49 -0400

    Creacion método getObjeto.

--- Expendedor/src/main/java/Classes/Deposito.java
+++ Expendedor/src/main/java/Classes/Deposito.java
@@ -2,24 +2,22 @@ package Classes;
 import java.util.ArrayList;
 
 /**
- * Se utiliza para simular un deposito en donde se pueden almacenar y sacar productos
- *<
- * @param <T> Parametro de tipo generico, define el tipo de deposito
+ * Se utiliza para simular un deposito en donde se pueden almacenar y sacar productos.
+ * @param <T> Parametro de tipo generico, define el tipo de deposito.
  */
 public class Deposito<T> {
     ArrayList<T> deposito;
 
     /**
-     * Constructor de la clase, inicializa la variable deposito
+     * Constructor de la clase, inicializa la variable deposito.
      */
     public Deposito(){
         deposito = new ArrayList<>();
     }
 
     /**
-     * metodo para obtener un elemento del ArrayList deposito
-     *
-     * @return un objeto generico, en caso de que el deposito esté vacío retorna vacío (null)
+     * Metodo para obtener un elemento del ArrayList deposito..
+     * @return un objeto generico, en caso de que el deposito esté vacío retorna vacío (null).
      */
     public T get(){
         if (deposito.isEmpty()){
@@ -32,21 +30,29 @@ public class Deposito<T> {
     }
 
     /**
-     * metodo para obtener el ArrayList deposito dentro de la clase Comprador
-     *
-     * @return objeto tipo Arraylist
+     * Metodo para obtener el ArrayList deposito dentro de la clase Comprador.
+     * @return objeto tipo Arraylist.
      */
     public ArrayList<T> returnArrayList(){
         return deposito;
     }
 
     /**
-     * metodo para añadir un objeto tipo generico a un deposito
-     *
-     * @param objeto Objeto tipo generico que representa un producto o una moneda
+     * Metodo para añadir un objeto tipo generico a un deposito.
+     * @param objeto Objeto tipo generico que representa un producto o una moneda.
      */
     public void add(T objeto){
         deposito.add(objeto);
     }
+
+    /**
+     * Getter que revisa cual es el objeto en el index i.
+     * @param i int que indica el index del ArrayList.
+     * @return objeto en el index.
+     */
+    public T getObjeto(int i){
+        return deposito.get(i);
+    }
+
 
 }
