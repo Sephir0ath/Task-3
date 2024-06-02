129f89e790bbacf0bace763701c0ed070d43582d
sshiro0 <francnunez2023@inf.udec.cl>
2024-06-02 18:14:03 -0400

    Pequeña adición de excepción en una línea de Expendedor. Implementación completa de los íconos en PaintComponent en sus debidas posiciones correctas en la ventana: miProducto comprado, vueltoTotal. Añadido su Javadoc. (añadir PanelBotonesMonedas y revisar).

--- Expendedor/src/main/java/Classes/Expendedor.java
+++ Expendedor/src/main/java/Classes/Expendedor.java
@@ -141,7 +141,7 @@ public class Expendedor {
                 }
             }
         }
-
+        throw new NoHayProductoException();
     }
 
     /**
--- Expendedor/src/main/java/Interfaz/PanelComprador.java
+++ Expendedor/src/main/java/Interfaz/PanelComprador.java
@@ -12,11 +12,11 @@ public class PanelComprador extends JPanel {
     private PanelBotonesMonedas BotonesMonedas;
 
     /**
-     * Constructor
+     * Constructor que personaliza el panel e inicializa el panelBotonesMoneda que contiene.
      */
     public PanelComprador() {
         super();
-        this.comprador = new Comprador();
+        comprador = new Comprador();
         setLayout(null);
         setOpaque(false);
         setBounds(500,0,400, 800);
@@ -34,24 +34,80 @@ public class PanelComprador extends JPanel {
     }
 
     /**
-     * Override del método paintComponent(g), personaliza visualmente el panel y su contenido.
+     * Override del método paintComponent(g), personaliza visualmente el panel y su contenido, pinta el fondo, el producto comprado y la cifra del vuelto total.
      * @param g the <code>Graphics</code> object to protect
      */
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.GRAY);
-        g.fillRect(0, 0, getWidth(), getHeight());
+        ImageIcon imageIcon = new ImageIcon(getClass().getClassLoader().getResource("panelComprador.png"));
+        g.drawImage(imageIcon.getImage(), 0, 0, null);
+        if (PanelComprador.getComprador().queConsumiste() == "CocaCola"){
+            g.drawImage(new ImageIcon(getClass().getClassLoader().getResource("miproductoCoca.png")).getImage(), 100, 220, null);
+        }
+        else if (PanelComprador.getComprador().queConsumiste() == "Sprite"){
+            g.drawImage(new ImageIcon(getClass().getClassLoader().getResource("miproductoSprite.png")).getImage(), 100, 220, null);
+        }
+        else if (PanelComprador.getComprador().queConsumiste() == "Fanta"){
+            g.drawImage(new ImageIcon(getClass().getClassLoader().getResource("miproductoFanta.png")).getImage(), 100, 220, null);
+        }
+        else if (PanelComprador.getComprador().queConsumiste() == "Snickers"){
+            g.drawImage(new ImageIcon(getClass().getClassLoader().getResource("miproductoSnickers.png")).getImage(), 100, 230, null);
+        }
+        else if (PanelComprador.getComprador().queConsumiste() == "Super8"){
+            g.drawImage(new ImageIcon(getClass().getClassLoader().getResource("miproductoSuper8.png")).getImage(), 100, 210, null);
+        }
+        else{
+            g.drawImage(new ImageIcon(getClass().getClassLoader().getResource("miproductoNone.png")).getImage(), 100, 230, null);
+        }
 
-        // Zona producto comprado
-        g.setColor(Color.BLUE);
-        g.fillRect(70, 210, 260, 300);
-        
-        // Zona vuelto (monedero que se le suma el vuelto), dibujar los items
-        g.setColor(Color.GREEN);
-        g.fillRect(70, 560, 260, 150);
-        
-        
-        //Poner suma del vuelto acumulado !!!!
+        try {
+            String vueltoString = Integer.toString(PanelComprador.getComprador().cuantoVuelto());
+            int posicionX = 0;
+            for (int i = 2; i < vueltoString.length(); i++) {
+                posicionX += 14;
+            }
+            for (int i = 0; i <= vueltoString.length(); i++) {
+                if (i == 0) {
+                    imageIcon = new ImageIcon(getClass().getClassLoader().getResource("numVueltoPeso.png"));
+                }
+                else {
+                    char numero = vueltoString.charAt(i - 1);
+                    if (numero == '0') {
+                        imageIcon = new ImageIcon(getClass().getClassLoader().getResource("numVuelto0.png"));
+                    }
+                    else if (numero == '1') {
+                        imageIcon = new ImageIcon(getClass().getClassLoader().getResource("numVuelto1.png"));
+                    }
+                    else if (numero == '2') {
+                        imageIcon = new ImageIcon(getClass().getClassLoader().getResource("numVuelto2.png"));
+                    }
+                    else if (numero == '3') {
+                        imageIcon = new ImageIcon(getClass().getClassLoader().getResource("numVuelto3.png"));
+                    }
+                    else if (numero == '4') {
+                        imageIcon = new ImageIcon(getClass().getClassLoader().getResource("numVuelto4.png"));
+                    }
+                    else if (numero == '5') {
+                        imageIcon = new ImageIcon(getClass().getClassLoader().getResource("numVuelto6.png"));
+                    }
+                    else if (numero == '6') {
+                        imageIcon = new ImageIcon(getClass().getClassLoader().getResource("numVuelto6.png"));
+                    }
+                    else if (numero == '7') {
+                        imageIcon = new ImageIcon(getClass().getClassLoader().getResource("numVuelto7.png"));
+                    }
+                    else if (numero == '8') {
+                        imageIcon = new ImageIcon(getClass().getClassLoader().getResource("numVuelto8.png"));
+                    }
+                    else if (numero == '9'){
+                        imageIcon = new ImageIcon(getClass().getClassLoader().getResource("numVuelto9.png"));
+                    }
+                }
+                g.drawImage(imageIcon.getImage(),186-posicionX + 14*i, 607,null);
+            }
+        }
+        catch (IndexOutOfBoundsException e) {
+        }
     }
 }
