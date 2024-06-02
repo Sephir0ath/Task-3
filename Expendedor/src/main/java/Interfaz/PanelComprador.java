856bb209f096238d320de0c38a8f46554f768237
sshiro0 <francnunez2023@inf.udec.cl>
2024-06-01 20:11:17 -0400

    Actualizaciones en PanelComprador y PanelExpendedor, cambios en la personalización y creaciones de instancias (pendiente: crear clases faltantes). Uso de paintComponent para declarar areas destinadas a paneles y botones.

--- Expendedor/src/main/java/Interfaz/PanelComprador.java
+++ Expendedor/src/main/java/Interfaz/PanelComprador.java
@@ -1,37 +1,57 @@
 package Interfaz;
 
 import Classes.*;
-
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Panel que muestra al comprador y su inventario.
+ */
 public class PanelComprador extends JPanel {
-    private Deposito<Moneda> depositoMonedas;
-    private PanelBotonesMonedas panelBotonesMonedas;
-    private PanelBotonesCompra panelBotonesCompra;
+    private static Comprador comprador;
+    private PanelBotonesMonedas BotonesMonedas;
 
-    public PanelComprador(){
+    /**
+     * Constructor
+     */
+    public PanelComprador() {
         super();
-        depositoMonedas = new Deposito<>();
-        panelBotonesCompra = new PanelBotonesCompra();
-        panelBotonesMonedas = new PanelBotonesMonedas(this.depositoMonedas);
+        this.comprador = new Comprador();
+        setLayout(null);
+        setOpaque(false);
+        setBounds(500,0,400, 800);
 
-        this.setLayout(new GridLayout(6, 1));
-        Label label = new Label();
-        label.setBackground(Color.GRAY);
-        this.add(label);
-        this.add(panelBotonesMonedas, BorderLayout.NORTH);
-        this.setPreferredSize(new Dimension(450, 800));
-
-        this.add(panelBotonesCompra);
+        BotonesMonedas = new PanelBotonesMonedas();
+        add(BotonesMonedas);
     }
 
+    /**
+     * Getter de comprador.
+     * @return comprador que compra el producto.
+     */
+    public static Comprador getComprador() {
+        return comprador;
+    }
 
+    /**
+     * Override del método paintComponent(g), personaliza visualmente el panel y su contenido.
+     * @param g the <code>Graphics</code> object to protect
+     */
+    @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
         g.setColor(Color.GRAY);
-        g.fillRect(0, 0, 450, 800);
+        g.fillRect(0, 0, getWidth(), getHeight());
 
+        // Zona producto comprado
+        g.setColor(Color.BLUE);
+        g.fillRect(70, 210, 260, 300);
+        
+        // Zona vuelto (monedero que se le suma el vuelto), dibujar los items
+        g.setColor(Color.GREEN);
+        g.fillRect(70, 560, 260, 150);
+        
+        
+        //Poner suma del vuelto acumulado !!!!
     }
 }
--- Expendedor/src/main/java/Interfaz/PanelExpendedor.java
+++ Expendedor/src/main/java/Interfaz/PanelExpendedor.java
@@ -1,28 +1,114 @@
 package Interfaz;
 
+import Classes.*;
 import javax.swing.*;
 import java.awt.*;
 
 public class PanelExpendedor extends JPanel {
+    private static Expendedor expendedor;
+
+	
+    /*private ButtonCoca coca;
+    private ButtonSprite sprite;
+    private ButtonFanta fanta;
+    private ButtonSnickers snickers;
+    private ButtonSuper8 super8;
+
+    private ButtonVuelto vuelto;
+    private PanelBotonesCompra compra;
+    private ButtonTomarProducto producto;
+    private PanelIngresarMoneda ingresoMonedas;
+    private PanelDepositoVuelto depositoVuelto;
+    private PanelDepositoMonedas depositoMonedas;
+    private PanelDepositoProductos depositoProductos;*/
+
     public PanelExpendedor(){
         super();
-        this.setLayout(new GridLayout());
-        this.setPreferredSize(new Dimension(450, 800));
 
+        expendedor = new Expendedor();
+
+        /*coca = new ButtonCoca();
+        sprite = new ButtonSprite();
+        fanta = new ButtonFanta();
+        snickers = new ButtonSnickers();
+        super8 = new ButtonSuper8();
+
+        vuelto = new ButtonVuelto();
+        producto = new ButtonTomarProducto();
+
+        compra = new PanelBotonesCompra();
+        ingresoMonedas = new PanelIngresarMoneda();
+        depositoVuelto = new PanelDepositoVuelto();
+        depositoMonedas = new PanelDepositoMonedas();
+        depositoProductos = new PanelDepositoProductos();*/
+
+        setLayout(null);
+        setBounds(0,0,500, 800);
+
+        /*add(coca);
+        add(sprite);
+        add(fanta);
+        add(snickers);
+        add(super8);
+
+        add(compra);
+
+        add(vuelto);
+        add(producto);
+        add(ingresoMonedas);
+        add(depositoVuelto);
+        add(depositoMonedas);
+        add(depositoProductos);
+
+          */
     }
 
-    protected void paintComponent(Graphics g){
+    public static void reinicioExpendedor() {
+        expendedor = new Expendedor();
+            }
+
+    @Override
+    public void paintComponent(Graphics g){
         super.paintComponent(g);
 
         // Pintar display de productos
         g.setColor(Color.RED);
-        g.fillRect(0, 0, 450, 600);
+        g.fillRect(0, 0, 600, 600);
 
         // Pintar zona sacar productos
         g.setColor(Color.DARK_GRAY);
-        g.fillRect(0, 600, 450, 200);
+        g.fillRect(0, 600, 600, 200);
 
+        // Zona productos
+        g.setColor(Color.BLACK);
+        g.fillRect(30, 120, 350, 400);
 
+        // Zona coca
+        g.setColor(Color.RED);
+        g.fillRect(70, 160, 70, 130);
+
+        // Zona fanta
+        g.setColor(Color.ORANGE);
+        g.fillRect(165, 160, 70, 130);
+
+        // Zona sprite
+        g.setColor(Color.GREEN);
+        g.fillRect(260, 160, 70, 130);
+
+        // Zona snickers
+        g.setColor(Color.CYAN);
+        g.fillRect(90, 340, 90, 130);
+
+        // Zona super8
+        g.setColor(Color.YELLOW);
+        g.fillRect(220, 340, 90, 130);
+
+        // Zona sacar producto
+        g.setColor(Color.BLACK);
+        g.fillRect(30, 635, 350, 100);
     }
 
+    public static Expendedor getExpendedor() {
+        return expendedor;
+    }
 }
