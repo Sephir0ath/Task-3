f6cea2b7867223b6d32bc79011d956f75bfe00e6
Mazulin <matijenven2004@gmail.com>
2024-06-01 21:05:35 -0400

    Actualización completa de ButtonCancelar y ButtonComprar, añadidas sus interacciones con el mouse, íconos de prueba y sus lógicas de funcionalidad.

--- Expendedor/src/main/java/Interfaz/ButtonCancelar.java
+++ Expendedor/src/main/java/Interfaz/ButtonCancelar.java
@@ -1,12 +1,87 @@
 package Interfaz;
 
+import Classes.*;
+import Excepciones.NoHayProductoException;
+import Excepciones.PagoIncorrectoException;
+import Excepciones.PagoInsuficienteException;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+/**
+ * Clase que cancela la compra de la expendedora.
+ */
 public class ButtonCancelar extends JButton {
-    public ButtonCancelar(){
-        super("Cancel");
 
-        this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+    /**
+     * Constructor que configura las características del botón.
+     */
+    public ButtonCancelar() {
+        super();
+        setOpaque(false);
+        setContentAreaFilled(false);
+        setBorderPainted(false);
+        setFocusPainted(false);
+        setBackground(new Color(0, 0, 0, 0));
+        setIcon(new ImageIcon(getClass().getClassLoader().getResource("rojo.png")));
+
+        addMouseListener(new MouseListener() {
+
+            /**
+             * Evento ejecutado al clickear el botón generado por defecto.
+             * @param e evento a ser procesado.
+             */
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            /**
+             * Evento ejecutado al presionar el botón. Concreta la compra con una compra nula para cancelar la compra.
+             * @param e evento a ser procesado.
+             */
+            @Override
+            public void mousePressed(MouseEvent e) {
+                setIcon(new ImageIcon(getClass().getClassLoader().getResource("morado.png")));
+                if (PanelIngresarMoneda.monedaYaIngresada()) {
+                    try {
+                        PanelExpendedor.getExpendedor().comprarProducto(PanelIngresarMoneda.getPago(), null);
+                    } catch (Exception a) {
+                        Window.frame().repaint();
+                    } finally {
+                        ButtonComprar.producto = null;
+                    }
+                }
+            }
+
+            /**
+             * Evento ejecutado al soltar el botón.
+             * @param e evento a ser procesado.
+             */
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                setIcon(new ImageIcon(getClass().getClassLoader().getResource("morado.png")));
+            }
+
+            /**
+             * Evento ejecutado al entrar el botón.
+             * @param e evento a ser procesado.
+             */
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                setIcon(new ImageIcon(getClass().getClassLoader().getResource("morado.png")));
+            }
+
+            /**
+             * Evento ejecutado al salir del botón.
+             * @param e evento a ser procesado.
+             */
+            @Override
+            public void mouseExited(MouseEvent e) {
+                setIcon(new ImageIcon(getClass().getClassLoader().getResource("rojo.png")));
+            }
+        });
     }
 }
--- Expendedor/src/main/java/Interfaz/ButtonComprar.java
+++ Expendedor/src/main/java/Interfaz/ButtonComprar.java
@@ -1,15 +1,96 @@
 package Interfaz;
 
+import Classes.*;
+import Excepciones.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class ButtonComprar extends JButton {
+    public static Productos producto = null;
+
+    /**
+     * Constructor que personaliza las características del botón.
+     */
     public ButtonComprar(){
-        super("Buy");
+        super();
+        setOpaque(false);
+        setContentAreaFilled(false);
+        setBorderPainted(false);
+        setFocusPainted(false);
+        setBackground(new Color(0, 0, 0, 0));
+        setIcon(new ImageIcon(getClass().getClassLoader().getResource("amarillo.png")));
 
-        this.setBackground(Color.YELLOW);
-        this.setForeground(Color.BLACK);
-        this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        addMouseListener(new MouseListener() {
 
+            /**
+             * Evento ejecutado al clickear el botón generado por defecto.
+             * @param e evento a ser procesado.
+             */
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            /**
+             * Evento ejecutado al presionar el botón. Compra el producto seleccionado.
+             * @param e evento a ser procesado.
+             */
+            @Override
+            public void mousePressed(MouseEvent e) {
+                setIcon(new ImageIcon(getClass().getClassLoader().getResource("rojo.png")));
+                if(ButtonTomarProducto.dispensadorVacio) try {
+                    PanelComprador.getComprador().comprar(PanelIngresarMoneda.getPago(), Productos.COCACOLA, PanelExpendedor.getExpendedor());
+                    ButtonTomarProducto.dispensadorVacio = false;
+                    // PanelExpendedor.actualizarTipText();
+                    Window.frame().repaint();
+                }
+                catch (NoHayProductoException f) {
+                    // Crear ventana emergente
+                }
+                catch (PagoIncorrectoException f) {
+                    // Crear ventana emergente
+                }
+                catch (PagoInsuficienteException f) {
+                    // Crear ventana emergente
+                }
+                finally {
+                    PanelIngresarMoneda.setPago(null);
+                    producto = null;
+                    Window.frame().repaint();
+                }
+                else {
+                    // Hay producto en el dispensador, crear ventana emergente
+                }
+            }
+
+            /**
+             * Evento ejecutado al soltar el botón.
+             * @param e evento a ser procesado.
+             */
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                setIcon(new ImageIcon(getClass().getClassLoader().getResource("rojo.png")));
+            }
+
+            /**
+             * Evento ejecutado al entrar el botón.
+             * @param e evento a ser procesado.
+             */
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                setIcon(new ImageIcon(getClass().getClassLoader().getResource("rojo.png")));
+            }
+
+            /**
+             * Evento ejecutado al salir del botón.
+             * @param e evento a ser procesado.
+             */
+            @Override
+            public void mouseExited(MouseEvent e) {
+                setIcon(new ImageIcon(getClass().getClassLoader().getResource("amarillo.png")));;
+            }
+        });
     }
 }
