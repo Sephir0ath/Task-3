a5a881181a068bb1d586a48162828b0f67f62928
Kingsephir <feliperaysz3@gmail.com>
2024-06-02 23:31:10 -0400

    Modificado texto en creación de instancia MessageWindow en clase ButtonComprar

--- Expendedor/src/main/java/Interfaz/ButtonComprar.java
+++ Expendedor/src/main/java/Interfaz/ButtonComprar.java
@@ -60,7 +60,7 @@ public class ButtonComprar extends JButton {
                     Window.frame().repaint();
                 }
                 else {
-                    new MessageWindow("Hay producto en el dispensador");
+                    new MessageWindow("Tome el producto que está debajo de la barra de productos");
                 }
             }
 
