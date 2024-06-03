3e7bea512d06fa9cf21c2359d94f0acadf99e261
Mazulin <matijenven2004@gmail.com>
2024-06-02 20:41:22 -0400

    Actualizado PanelDepositoMonedas, muestra las monedas en el depósito (Revisar: Tiene errores en el paintComponent).

--- Expendedor/src/main/java/Interfaz/PanelDepositoMonedas.java
+++ Expendedor/src/main/java/Interfaz/PanelDepositoMonedas.java
@@ -1,4 +1,50 @@
 package Interfaz;
 
-public class PanelDepositoMonedas {
+import Classes.*;
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Panel que muestra el depósito de monedas del expendedor.
+ */
+
+public class PanelDepositoMonedas extends JPanel{
+    int limiteSuperior = 1;
+    /**
+     * Constructor que personaliza el panel.
+     */
+    public PanelDepositoMonedas() {
+        super();
+        setLayout(null);
+        setOpaque(false);
+        setBounds(270, 644, 199, 90);
+    }
+
+
+    /**
+     * Override de paintComponent(g) para dibujar las monedas que están en el depósito de monedas.
+     * @param g the <code>Graphics</code> object to protect
+     */
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (int i=0; i < PanelExpendedor.getExpendedor().depositoSize("depositoMonedas"); i++){
+            if (i == 21*limiteSuperior) {
+                limiteSuperior++;
+            }
+            Moneda moneda = PanelExpendedor.getExpendedor().getMoneda(i, "depositoMonedas");
+            if (moneda.getValor() == 100) {
+                g.drawImage(new ImageIcon(getClass().getClassLoader().getResource("moneda100Depositada.png")).getImage(), (limiteSuperior-1)*20 ,81-(4*i), null );
+            }
+            else if (moneda.getValor() == 500) {
+                g.drawImage(new ImageIcon(getClass().getClassLoader().getResource("moneda500Depositada.png")).getImage(),(limiteSuperior-1)*20 ,81-(4*i), null );
+            }
+            else if (moneda.getValor() == 1000) {
+                g.drawImage(new ImageIcon(getClass().getClassLoader().getResource("moneda1000Depositada.png")).getImage(),(limiteSuperior-1)*20 ,81-(4*i), null );
+            }
+            else if (moneda.getValor() == 1500) {
+                g.drawImage(new ImageIcon(getClass().getClassLoader().getResource("moneda1500Depositada.png")).getImage(),(limiteSuperior-1)*20 ,81-(4*i), null );
+            }
+        }
+    }
 }
